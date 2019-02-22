6946f4eb1bb64c95ba532eb6da33b41cfb402e98
Luis Fernando <lf.silva@outlook.com>
2019-02-22 17:03:50 -0300

    starting the spring boot application

---
+++ _server/src/main/java/br/com/vac_app/endpoints/LoginEndpointUnitTest.java
@@ -0,0 +1,15 @@
+package br.com.vac_app.endpoints;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginEndpointUnitTest {
+
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String home() {
+        return "Hello";
+    }
+}
---
+++ _server/src/test/java/br/com/vac_app/endpoints/LoginEndpointUnitTest.java
@@ -0,0 +1,4 @@
+package br.com.vac_app.endpoints;
+
+public class LoginEndpointUnitTest {
+}
